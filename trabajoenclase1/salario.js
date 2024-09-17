428932b1136e9af85701a8441f3e2223f600433c
trianiyuan <triani001@gmail.com>
2024-09-16 18:34:53 -0600

    trabajoenclase1

--- trabajoenclase1/salario.js
+++ trabajoenclase1/salario.js
@@ -1,19 +1,21 @@
-// Función para calcular el salario total
 function calculateTotalSalary(baseSalary, overtimeRate, overtimeHours) {
-    // Calcular el salario por horas extras
+    
     const overtimePay = overtimeRate * overtimeHours;
 
-    // Calcular el salario total
+    
     const totalSalary = baseSalary + overtimePay;
 
     return totalSalary;
 }
 
-// Ejemplo de uso
-const baseSalary = 2000; // Salario base del empleado
-const overtimeRate = 20; // Monto que cobra por hora extra
-const overtimeHours = 15; // Cantidad de horas extra trabajadas
+const baseSalary = 2000; 
+const overtimeRate = 20;
+const overtimeHours = 15; 
 
 const totalSalary = calculateTotalSalary(baseSalary, overtimeRate, overtimeHours);
 
 console.log(`El salario total del empleado es: $${totalSalary}`);
+
+
+let salario=200000;
+let tarifa=250;
